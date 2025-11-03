2025c50e3ddc883eea438544d3386415269f5c19
RenanCruz7 <renancruz199@gmail.com>
2025-11-03 00:00:13 -0300

    Configure AutoMapper and set up ProductService with MySQL DbContext

--- ShopSim/Program.cs
+++ ShopSim/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using ShopSim.Data;
+using ShopSim.Profiles;
+using ShopSim.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// AutoMapper configuration
+builder.Services.AddAutoMapper(typeof(ProductProfile));
+
+// Injeção de dependência do serviço de produtos
+builder.Services.AddScoped<IProductService, ProductService>();
+
+// Configuração do DbContext com SQL Server
+var connectionString = builder.Configuration.GetConnectionString("DriverConnection");
+builder.Services.AddDbContext<ShopSimContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
