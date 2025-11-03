90fd84cd8dc5ad7802946dcbc9ff7d09b919f968
RenanCruz7 <renancruz199@gmail.com>
2025-11-03 19:05:17 -0300

    Add initial migration and design-time DbContext factory for MySQL

---
+++ ShopSim/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ShopSim.Data;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ShopSimContext>
+{
+    public ShopSimContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<ShopSimContext>();
+        
+        // Para design-time, use localhost já que o comando roda fora do Docker
+        builder.UseMySql(
+            "Server=localhost;Port=3307;Database=ShopSim;Uid=appuser;Pwd=AppPassword123!;",
+            new MySqlServerVersion(new Version(8, 0, 33))
+        );
+
+        return new ShopSimContext(builder.Options);
+    }
+}
